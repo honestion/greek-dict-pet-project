2c90730369386dff669cc69c0cc3cefaa453020d
seecue <second2secue@gmail.com>
2026-04-11 13:52:36 +0300

    Menu file added

---
+++ start_menu.py
@@ -0,0 +1,8 @@
+def start_menu():
+    print('=' * 38)
+    print('Now you are in the main menu. You can:')
+    print('1. Show all your greek words')
+    print('2. Add new word')
+    print('3. Delete word')
+    print('Press number you need')
+    print('=' * 38)
