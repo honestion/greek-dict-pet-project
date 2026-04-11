945d7be91ffda51a4dc8e77bd9a4ce6612816bc3
seecue <second2secue@gmail.com>
2026-04-11 16:36:05 +0300

    main fuction if whole programm created

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from start_menu import start_menu
+from add_word import add_word
+from delete_word import delete_word
+from show_words import show_words
 
 def main():
-    start_menu()
-    action = int(input())
+    while True:
+        start_menu()
+        action = int(input()) # action request to get what to do
+        # checking key actions
+        match action:
+            case 1:
+                show_words()
+            case 2:
+                add_word()
+            case 3:
+                delete_word()
+            case 4:
+                return False
+            case _:
+                print('Type num from 1 to 4, nothing else')
 
 main()
