6971411da928ba7292d6d00b105d1fd93e7aa4b5
seecue <second2secue@gmail.com>
2026-04-11 16:31:56 +0300

    start menu function created

--- start_menu.py
+++ start_menu.py
@@ -1,8 +1,9 @@
 def start_menu():
     print('=' * 38)
-    print('Now you are in the main menu. You can:')
+    print(' '* 13, 'Main menu', ' ' * 13)
     print('1. Show all your greek words')
     print('2. Add new word')
     print('3. Delete word')
+    print('4. Exit')
     print('Press number you need')
     print('=' * 38)
